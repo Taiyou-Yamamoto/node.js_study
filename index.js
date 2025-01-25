c1fc3e39a9453fb8e791ef1f593bee1ba8ab4e55
山本　太陽 <yamamototaiyou.811@gmail.com>
2025-01-25 17:37:09 +0900

    簡単なapi

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3010;
+app.use(express.json()); //json形式を使う
 
 app.listen(port, () => {
   console.log(`サーバー起動！ http://localhost:${port}`);
@@ -22,9 +23,28 @@ app.get('/api/customers', (req, res) => {
   res.send(customers);
 });
 
-app.post('apt/customers', (req, res) => {
+app.post('/api/customers', (req, res) => {
   const customer = {
     title: req.body.title,
     id: customers.length + 1,
   };
+
+  customers.push(customer);
+  res.send(customer);
+});
+
+// 情報の更新
+app.put('/api/customers/:id', (req, res) => {
+  const customer = customers.find((c) => c.id === parseInt(req.params.id));
+  customer.title = req.body.title;
+  res.send(customers);
+});
+
+app.delete('/api/customers/:id', (req, res) => {
+  const customer = customers.find((c) => c.id === parseInt(req.params.id));
+  customer.title = req.body.title;
+
+  const index = customers.indexOf(customer);
+  customers.splice(index, 1);
+  res.send(customers);
 });
