8194bcaed7b7cc94756f493192d9ba070bc078ea
山本　太陽 <yamamototaiyou.811@gmail.com>
2025-01-26 23:36:14 +0900

    add dotenv

--- todo_app/app.js
+++ todo_app/app.js
@@ -3,6 +3,7 @@ const app = express(); //express関数をappから呼び出せるようにする
 const port = 5010;
 const taskRoute = require('./routes/tasks');
 const connectDB = require('./db/connect');
+require('dotenv').config();
 
 //ルーティング設定
 app.use('/api/v1/tasks', taskRoute);
@@ -10,9 +11,7 @@ app.use('/api/v1/tasks', taskRoute);
 // データベースと接続
 const start = async () => {
   try {
-    await connectDB(
-      'mongodb+srv://Taiyo:Taiyou811@cluster0.szenfc6.mongodb.net/?retryWrites=true&w=majority&appName=todoapp'
-    );
+    await connectDB(process.env.MONGO_URL);
     app.listen(port, console.log('サーバーが起動しました'));
   } catch (error) {
     console.log(error);
