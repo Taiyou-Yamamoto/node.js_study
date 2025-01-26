8a8a2f35579a62d148dbdcf75edbcc53e4f947be
山本　太陽 <yamamototaiyou.811@gmail.com>
2025-01-27 00:31:25 +0900

    add get_singleTask

--- todo_app/controllers/tasks.js
+++ todo_app/controllers/tasks.js
@@ -1,8 +1,14 @@
 const Task = require('../models/Task');
 
-const getAllTasks = (req, res) => {
-  res.send('タスクを全て取得しました');
+const getAllTasks = async (req, res) => {
+  try {
+    const allTask = await Task.find({});
+    res.status(200).json(allTask);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 };
+
 const createTask = async (req, res) => {
   try {
     const createTask = await Task.create(req.body);
@@ -11,13 +17,24 @@ const createTask = async (req, res) => {
     res.status(500).json(error);
   }
 };
-const getSingleTask = (req, res) => {
-  res.send(`${req.params.id}タスクを取得しました`);
+
+const getSingleTask = async (req, res) => {
+  try {
+    const getSingleTask = await Task.findOne({ _id: req.params.id }).exec();
+    if (!getSingleTask) {
+      return res.status(404).json(`_id:${req.params.id}は存在しません`);
+    }
+    res.status(200).json(getSingleTask);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 };
-const updateTask = (req, res) => {
+
+const updateTask = async (req, res) => {
   res.send(`${req.params.id}タスクを更新しました`);
 };
-const deleteTask = (req, res) => {
+
+const deleteTask = async (req, res) => {
   res.send(`${req.params.id}タスクを削除しました`);
 };
 
