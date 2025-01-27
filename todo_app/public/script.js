aa7d1d5a2d4b6f41048ff4f2843c4f8b59ab4a5b
山本　太陽 <yamamototaiyou.811@gmail.com>
2025-01-27 21:09:24 +0900

    これ以上はバニラjavascriptの学習になるため終了

--- todo_app/public/script.js
+++ todo_app/public/script.js
@@ -1,6 +1,7 @@
 const tasksDOM = document.querySelector('.tasks');
 const formDOM = document.querySelector('.task-form');
 const taskInputDOM = document.querySelector('.task-input');
+const formAlertDOM = document.querySelector('.form-alert');
 
 const showTasks = async () => {
   try {
@@ -8,6 +9,11 @@ const showTasks = async () => {
     const { data: tasks } = await axios.get('/api/v1/tasks');
     console.log(tasks);
 
+    // タスクが一つもない時
+    if (tasks.length === 0) {
+      tasksDOM.innerHTML = `<h5 class="empty-list">何もないよ...</h5>`;
+      return;
+    }
     const allTasks = tasks
       .map((task) => {
         const { completed, _id, name } = task;
@@ -21,7 +27,7 @@ const showTasks = async () => {
     <a href="#" class="edit-link">
       <i class="fas fa-edit"></i>
     </a>
-    <button type="button" class="delete-btn">
+    <button type="button" class="delete-btn" data-id="${_id}">
       <i class="fas fa-trash"></i>
     </button>
   </div>
@@ -47,7 +53,33 @@ formDOM.addEventListener('submit', async (event) => {
     await axios.post('/api/v1/tasks', { name: name });
     showTasks();
     taskInputDOM.value = '';
+    formAlertDOM.style.display = 'block';
+    formAlertDOM.textContent = 'タスクを追加しました';
+    formAlertDOM.classList.add = 'text-success';
   } catch (err) {
     console.log(err);
+    formAlertDOM.style.display = 'block';
+    formAlertDOM.innerHTML = '無効です。もう一度やり直してください';
+  }
+
+  setTimeout(() => {
+    formAlertDOM.style.display = 'none';
+  }, 3000);
+});
+
+// タスクを削除
+
+tasksDOM.addEventListener('click', async (event) => {
+  const element = event.target;
+  console.log(element);
+  if (element.parentElement.classList.contains('delete-btn')) {
+    const id = element.parentElement.dataset.id;
+    console.log(id);
+    try {
+      await axios.delete(`/api/v1/tasks/${id}`);
+      showTasks();
+    } catch (err) {
+      console.log(err);
+    }
   }
 });
